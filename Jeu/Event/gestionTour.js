ca5e710ca696e3e9e5c683df651cd64202b44b88
DE POTTER Quentin <depotter@phoenix.iutrs.unistra.fr>
2019-04-05 09:28:48 +0200

    integration conseil

--- Jeu/Event/gestionTour.js
+++ Jeu/Event/gestionTour.js
@@ -5,38 +5,54 @@ const sfm = require('../Main/saveFileManagement.js');
 var tour = 0 ;
 
 exports.gTours = function(message, partie) {
-  myBot.clear(message);
-  const embed = new Discord.RichEmbed()
-  .setTitle('Année ' + partie.annee + ' !')
-  .setTimestamp()
-  .addField('Meteo', 'Un temps légèrement nuagueux')
-  .setFooter('Cliquez sur ➡ pour passer le tour');
-  partie.annee += 5;
-  sfm.save(partie.player, partie);
-  message.channel.send({ embed })
-  .then(async function(mes) {
-    await mes.react('➡');
-  });
+  sendNouvelleAnnee(message, partie) ;
 
+  partie.aviAristo -= 0.14 ;
+  partie.aviClerge += 0.01 ;
+  partie.aviArmee += 0.08 ;
 
-  if(tour >1){
-    partie.aviAristo -= 0.12 ;
-    partie.aviClerge += 0.01 ;
-    partie.aviArmee += 0.27 ;
-
-    check(partie) ;
-  }
+  checkOpi(partie) ;
 
   // CONSEIL
   myBot.writeConseil(message, partie) ;
 
+
+  // CLEAR
+  myBot.clear()
+
+  sfm.save(partie.player, partie);
+
   tour++ ;
 };
 
+function sendNouvelleAnnee(message, partie){
+    myBot.clear(message);
+    const embed = new Discord.RichEmbed()
+    .setTitle('Année ' + partie.annee + ' !')
+    .setTimestamp()
+    .addField('Meteo', 'Un temps légèrement nuagueux')
+    .setFooter('Cliquez sur ➡ pour passer le tour');
+    partie.annee += 5;
+    message.channel.send({ embed })
+    .then(async function(mes) {
+      await mes.react('➡');
+    });
 
-function check(partie){
+}
+
+function checkOpi(partie){
   if(partie.aviClerge>1)
     partie.aviClerge = 1 ;
   if(partie.aviClerge<0)
     partie.aviClerge = 0 ;
+
+  if(partie.aviArmee>1)
+    partie.aviArmee = 1 ;
+  if(partie.aviArmee<0)
+    partie.aviArmee = 0 ;
+
+  if(partie.aviAristo>1)
+    partie.aviAristo = 1 ;
+  if(partie.aviAristo<0)
+    partie.aviAristo = 0 ;
 }
