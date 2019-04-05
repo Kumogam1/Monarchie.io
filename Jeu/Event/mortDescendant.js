882857d58b8b2b1ee644e5420a28bfd8d31cceee
Edouard Gu <e.gu@unistra.fr>
2019-04-05 13:44:48 +0200

    correction de bug

--- Jeu/Event/mortDescendant.js
+++ Jeu/Event/mortDescendant.js
@@ -1,6 +1,7 @@
 const sfm = require('../Main/saveFileManagement.js');
 
 exports.mortDescendant = function(partie, ind) {
-  partie.enfants.splice(ind);
+  partie.enfants.splice(ind, 1);
+  console.log(partie.enfants);
   sfm.save(partie.player, partie);
 };
